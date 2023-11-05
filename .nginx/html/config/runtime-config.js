dab20b381aae661a4f7a2c661369498c959b9d41
Nguyen Van Long (K15 HL) <longnvhe151244@fpt.edu.vn>
2023-11-05 16:00:01 +0700

    resolve docker for prod

--- .nginx/html/config/runtime-config.js
+++
@@ -1,1 +0,0 @@
-window.__RUNTIME_CONFIG__ = {"VITE_API_SERVER_URL":"https://api-vms.azurewebsites.net/api/v1","VITE_BASE_PATH":""};
--- vite.config.ts
+++ vite.config.ts
@@ -1,57 +1,57 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { constants, promises as fsp } from "fs";
+import { constants, promises as fsp } from 'fs'
 import commonjs from '@rollup/plugin-commonjs'
 import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5'
 import { createRequire } from 'node:module'
 
-const require = createRequire( import.meta.url )
+const require = createRequire(import.meta.url)
 
 
 async function myPlugin(envConfig, envFile) {
-  const runtimeConfig: any = {};
-  let envConfigExists = true;
+  const runtimeConfig: any = {}
+  let envConfigExists = true
   try {
-    await fsp.access(envConfig, constants.F_OK);
+    await fsp.access(envConfig, constants.F_OK)
   } catch (err) {
-    console.log(`${envConfig} does not exist. Creating one...`);
-    envConfigExists = false;
+    console.log(`${envConfig} does not exist. Creating one...`)
+    envConfigExists = false
   }
 
   if (envConfigExists) {
-    await fsp.unlink(envConfig);
+    await fsp.unlink(envConfig)
   }
   try {
-    await fsp.access(envFile, constants.F_OK);
+    await fsp.access(envFile, constants.F_OK)
   } catch (err) {
-    throw err;
+    throw err
   }
 
-  const content = await fsp.readFile(envFile, 'utf8');
+  const content = await fsp.readFile(envFile, 'utf8')
 
   content.split(/\r?\n/)
     .map((line: any) => line.split('#')[0].trim())
     .forEach((line: any) => {
-      const equalSignIndex = line.indexOf('=');
-      const lengthOfString = line.length;
+      const equalSignIndex = line.indexOf('=')
+      const lengthOfString = line.length
 
       if (equalSignIndex !== -1) {
-        const key = line.slice(0, equalSignIndex);
-        const value = line.slice(equalSignIndex + 1, lengthOfString);
-        runtimeConfig[key] = value;
+        const key = line.slice(0, equalSignIndex)
+        const value = line.slice(equalSignIndex + 1, lengthOfString)
+        runtimeConfig[key] = value
       }
-    });
+    })
 
   if (!Object.keys(runtimeConfig).length) {
     throw new Error(
-      'Could not generate runtime config. Check your .env format!',
-    );
+      'Could not generate runtime config. Check your .env format!'
+    )
   }
 
-  const result = `window.__RUNTIME_CONFIG__ = ${JSON.stringify(runtimeConfig)};`;
+  const result = `window.__RUNTIME_CONFIG__ = ${JSON.stringify(runtimeConfig)};`
 
-  await fsp.writeFile(envConfig, result, 'utf-8');
+  await fsp.writeFile(envConfig, result, 'utf-8')
 
 }
 
@@ -61,30 +61,30 @@ export default defineConfig(({ mode }) => {
     base: process.env.BASE || '',
     plugins: [
       react(),
-      ckeditor5( { theme: require.resolve( '@ckeditor/ckeditor5-theme-lark' ) } )
+      ckeditor5({ theme: require.resolve('@ckeditor/ckeditor5-theme-lark') })
     ],
     optimizeDeps: {
-      include: [ ]
+      include: []
     },
     build: {
       plugins: [
-        myPlugin('./public/config/runtime-config.js', `./.env.${mode}`)
+        myPlugin('./public/runtime-config.js', `./.env.${mode}`)
       ],
       rollupOptions: {
-        plugins: [commonjs()],
+        plugins: [commonjs()]
       },
       commonjsOptions: {
-        exclude: [/./],
-      },
+        exclude: [/./]
+      }
     },
     server: {
-      port: 3000,
+      port: 3000
     },
     resolve: {
       alias: {
-        '~': path.resolve(__dirname, './src'),
-      },
-    },
+        '~': path.resolve(__dirname, './src')
+      }
+    }
   }
 })
 
